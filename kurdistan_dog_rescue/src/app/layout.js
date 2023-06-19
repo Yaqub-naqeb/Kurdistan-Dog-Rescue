62e3d132615f4d8c98c728c7f3d65647470f491d
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-06-19 21:08:58 +0300

    simple navbar

--- kurdistan_dog_rescue/src/app/layout.js
+++ kurdistan_dog_rescue/src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from '../../components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} >
+<div className='px-10 py-5'>
+
+
+<Navbar/>
+          {children}
+
+</div>
+        </body>
     </html>
   )
 }
