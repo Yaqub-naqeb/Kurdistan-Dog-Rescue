ddb5e409332c6b160ba7f410b5cbb6d41120b69b
alan-analyst <alananalyst00@gmail.com>
2023-06-20 21:19:29 +0300

    media file configurations and more back-end funcionality

--- adopt_app/models.py
+++ adopt_app/models.py
@@ -5,7 +5,7 @@ class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    picture = models.ImageField(null=True, blank=True, default='default.png')
+    picture = models.ImageField(null=True, blank=True, default='default.png', upload_to='dog_pictures/')
     gender = models.CharField(max_length=10)
     age = models.IntegerField()
     size = models.CharField(max_length=10)
