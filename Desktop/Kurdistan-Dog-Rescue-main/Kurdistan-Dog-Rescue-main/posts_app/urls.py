311240d865478dfd296cd6b6ce9e25e7cc168214
yaqwb <105121706+Yaqub-naqeb@users.noreply.github.com>
2023-06-21 19:44:51 +0300

    Delete Desktop/Kurdistan-Dog-Rescue-main directory

--- Desktop/Kurdistan-Dog-Rescue-main/Kurdistan-Dog-Rescue-main/posts_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.posts, name='success_stories'),
-    path('<slug:slug>', views.post_detail, name='post_detail'),
-]
